cce1c2cb56145e4ab4d6edcfbb521106f82447c1
Gan Zixuan <zixuan.gan@rwth-aachen.de>
2017-06-25 19:59:13 +0200

    add read training data

--- para.py
+++ para.py
@@ -1,9 +1,12 @@
 class Para:
 	sourceVocabFilePath = "data/sourceVocab"
 	targetVocabFilePath = "data/engClass"
+	trainingDataFilePath = "data/dev"
 
 
 	def GetSourceVocabFilePath(self):
 		return self.sourceVocabFilePath
 	def GetTargetVocabFilePath(self):
 		return self.targetVocabFilePath
+	def GetTrainingDataFilePath(self):
+		return self.trainingDataFilePath
--- readData.py
+++ readData.py
@@ -2,6 +2,11 @@
 import sys
 import para
 import os
+class SentencePair:
+
+	_source = []
+	_target = []
+	_targetClass = [] 
 
 
 
@@ -10,11 +15,13 @@ class ReadData:
 	# these parameters should be initialized at one place
 	sourceVocabFilePath = ""
 	targetVocabFilePath = ""
+	trainingDataFilePath = ""
 
 	# these are inner parameters
 	sourceVocab = []
 	targetVocab = []
 	targetClass = []
+	trainingSentence = []
 
 	allert = 0
 
@@ -32,14 +39,23 @@ class ReadData:
 		self.targetVocabFilePath = os.path.join(os.path.dirname(__file__), parameters.GetTargetVocabFilePath())
 		self.readTargetDictionaryClass(self.targetVocabFilePath)
 
+		self.trainingDataFilePath = os.path.join(os.path.dirname(__file__), parameters.GetTrainingDataFilePath())
+		self.readTrainData(self.trainingDataFilePath)
+
+
+		if self.allert != 0 :
+			print('insufficient input data file')
+		else:
+			print('data ready')
+
 	def readSourceDictionary( self, filePath ):
 		try:
 			file = open(filePath, "r")
 			for line in file:
 				self.sourceVocab.append(line.rstrip())
 		except IOError as err:
-			print("source vocabulary files not found")
-			allert += 1
+			print("source vocabulary files do not exist")
+			self.allert += 1
 
 	def readTargetDictionaryClass( self, filePath ):
 		try:
@@ -51,11 +67,35 @@ class ReadData:
 				self.targetVocab.append(item[0].rstrip())
 				self.targetClass.append(item[2].rstrip())
 		except IOError as err:
-			print("source vocabulary files not found")
-			allert += 1
+			print("target vocabulary files do not exist")
+			self.allert += 1
 
+	def readTrainData(self, filePath):
+		try:
+			file = open(filePath, "r")
+			for line in file:
+				item = []
+				sentencePair = SentencePair()
+				for sentence in line.split('#'):
+					item.append(sentence)
+				for word in item[0].rstrip().split(' '):
+					sentencePair._source.append(self.sourceVocab.index(word))
+				for word in item[1].rstrip().split(' '):
+					sentencePair._target.append(self.targetVocab.index(word))
+					sentencePair._targetClass.append(self.targetClass[self.targetVocab.index(word)])
+				print(sentencePair._source)
+				print(item[0])
+				self.trainingSentence.append(sentencePair)
+
+		except IOError as err:
+			print("training files do not exist")
+			self.allert += 1
+		except ValueError as err:
+			print("abnormal input words")
 
 
 
 
 a = ReadData()
+
+
